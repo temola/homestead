fefbae88580980ae77ec639146fa2d8d2eb8b489
tomemola <temola@millikin.edu>
2018-10-16 13:12:04 -0500

    tried to use a different matchheight function

--- cyan/assets/js/custom.js
+++ cyan/assets/js/custom.js
@@ -1,3 +1,40 @@
-$(function() {
-	$('.special').matchHeight;
+//
+//	Equal height - by Lewi Hussey
+//
+
+var matchHeight = function () {
+
+	function init() {
+		eventListeners();
+		matchHeight();
+	}
+
+	function eventListeners(){
+		$(window).on('resize', function() {
+			matchHeight();
+		});
+	}
+
+	function matchHeight(){
+		var groupName = $('[data-match-height]');
+		var groupHeights = [];
+
+		groupName.css('min-height', 'auto');
+
+		groupName.each(function() {
+			groupHeights.push($(this).outerHeight());
+		});
+
+		var maxHeight = Math.max.apply(null, groupHeights);
+		groupName.css('min-height', maxHeight);
+	};
+
+	return {
+		init: init
+	};
+
+} ();
+
+$(document).ready(function() {
+	matchHeight.init();
 });
