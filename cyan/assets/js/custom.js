c5a3dd83b4f42f3c1fd5ec25655e4e13dac440f9
tomemola <temola@millikin.edu>
2018-10-18 14:15:55 -0500

    updated matchheight js

--- cyan/assets/js/custom.js
+++ cyan/assets/js/custom.js
@@ -35,6 +35,26 @@ var matchHeight = function () {
 
 } ();
 
+function matchHeigh2t(){
+	var group2 = $('[data-match-height]');
+	var group2Heights = [];
+
+	group2.css('min-height', 'auto');
+
+	group2.each(function() {
+		group2Heights.push($(this).outerHeight());
+	});
+
+	var maxHeight = Math.max.apply(null, group2Heights);
+	group2.css('min-height', maxHeight);
+};
+
+return {
+	init: init
+};
+
+} ();
+
 $(document).ready(function() {
 	matchHeight.init();
 });
